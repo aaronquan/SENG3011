56a236a225bae82b02195ecefce2feb21b9df12f
pauldong <z5020383@student.unsw.edu.au>
2016-03-21 13:40:46 +1100

    Port change

--- app.js
+++ app.js
@@ -53,5 +53,5 @@ app.use(function(err, req, res, next) {
 //end error handling
 
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 3000);
 console.log('API is running on port '+process.env.PORT);
