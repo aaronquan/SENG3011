d6c87aacb2f57b18ec16d6e46da94e767a76b549
Aaron Quan <z5020387@zmail.unsw.edu.au>
2016-04-05 23:19:49 +1000

    put some into in releases
    in desperate need of formatting

--- views/javascript/appNg.js
+++ views/javascript/appNg.js
@@ -12,23 +12,49 @@ app.config(function($routeProvider){
 });
 
 app.controller('releasesController', function(){
-	this.release = info;
+	this.releases = info;
+	this.cVersion = '1.0';
 });
 
-var info = {version: '1.0',
-	date: '5/04/2016',
-	new_features: 
-	    {new_commands: 
-	    	[{name:'/query',
-	    	 description: 'receives a post request with conditions and returns the specified news tuples.',
-	    	 output_type: 'application/json',
-	    	 schema: []},
+var info = [
+	{
+		version: '1.0',
+		date: '6/04/2016',
+		new_routes: 
+			[{name:'/query',
+			 description: 'receives a post request with conditions and returns the specified news tuples',
+			 output_type: 'application/json',
+			 schema:
+			 	[
+			 		'date: timestamp when the news was posted - Date',
+			 		'headline: the news headline - String',
+			 		'body: contents of the news - String',
+			 		'tpc_list: topic codes associated with the news - [String]',
+			 		'instr_list: instrument codes associated with the news - [String]'
+				]
+			 },
 			 {name: '/news',
 			 description: 'returns the full database of news',
 			 output_type: 'application/json',
-	    	 schema: []}
-	    	]
-	    },
-	comments: ['Adds and parses news file source into our MongoDB database into appropriate fields']
-	}
+			 schema: []
+			 },
+			 {name: '/source',
+			  description: 'displays the text file from where the database was created from',
+			  output_type: 'text/html',
+			  schema: []
+			 }
+			],
+		implemented: ['Database construction from news text file', 
+					  'Query by date range',
+					  'Query using topic and instrument codes',
+					 ],
+		differences: []
+		/*
+	},
+	{
+		version: '1.1',
+		date: '',
+		implemented: ['unreleased']
+		*/
+	}]
 
