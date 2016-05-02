a8ce679d34195523e4c1b971c38fd5ef74458154
aaronquan <z5020387@zmail.unsw.edu.au>
2016-05-02 16:48:29 +1000

    testing

--- tests/autotests.js
+++ tests/autotests.js
@@ -2,6 +2,9 @@ var mongoose = require('mongoose');
 var request = require('request');
 var async = require('async');
 
+//for the 
+//mongoose.connect("mongodb://aaronq:newsapi@ds015508.mlab.com:15508/seng3011-news");
+
 var News = mongoose.model('News');
 var Query = mongoose.model('News');
 
@@ -33,28 +36,55 @@ var testQueries = [
     {
         "start_date": "2015-10-01T00:00:00.092Z",
         "end_date": "2015-10-01T00:06:00.000Z",
-        "instr_list": ["KRW=","KREXGR=ECI"],
+        "instr_list": ["KRW="],
         "tpc_list": []
     },
     {
     	"start_date": "2015-10-01T00:00:00.092Z",
         "end_date": "2015-10-01T00:09:00.000Z",
         "instr_list": [],
-        "tpc_list": ['GEN', 'ECI']
+        "tpc_list": ['GEN']
     },
     {
     	"start_date": "2015-10-01T00:00:00.092Z",
         "end_date": "2015-10-01T00:09:00.000Z",
         "instr_list": ['KRW='],
         "tpc_list": ['GEN', 'ECI']
-    }
+    },
+    {
+    	"start_date": "2015-10-01T00:03:00.092Z",
+        "end_date": "2015-10-01T00:10:00.000Z",
+        "instr_list": ["OCDO.L","GLEN.L","BEG.L","CVT.AX","STL.AX"],
+        "tpc_list": ["GEN", "ECI", "MV", "TRD", "LEN"]	
+    },
+    {
+    	"start_date": "2015-10-01T00:02:00.092Z",
+        "end_date": "2015-10-01T00:04:00.000Z",
+        "instr_list": ["OCDO.L","GLEN.L","BEG.L","CVT.AX","STL.AX"],
+        "tpc_list": ["GEN", "ECI", "MV", "TRD", "LEN", "WASH"]	
+    },
+    {
+    	"start_date": "2016-10-01T00:02:00.092Z",
+        "end_date": "2016-10-01T00:04:00.000Z",
+        "instr_list": [],
+        "tpc_list": []	
+    },
+    {
+    	"start_date": "2016-10-01T00:02:00.092Z",
+        "end_date": "2015-10-01T00:04:00.000Z",
+        "instr_list": [],
+        "tpc_list": ["MV", "TRD", "LEN", "WASH"]	
+    },
+    {
+    	"start_date": "2015-10-01T00:02:00.092Z",
+        "end_date": "2015-10-01T00:04:00.000Z",
+        "instr_list": [],
+        "tpc_list": ["GENO"]	
+    },
 ];
 
 var tester = function(callback){
 	var log = [];
-	randomQuery(function(query){
-		console.log(query);
-	});
 	request(allNews, function(err, res, fullNews){
 		fullNews = JSON.parse(fullNews);
 		async.forEachOfSeries(testQueries, 
@@ -79,17 +109,21 @@ var tester = function(callback){
 		);
 	});
 };
+
+//matches the api query response
+
 function testQuery(query, body, allNews, callback){
 	var s_date = new Date(query['start_date']);
 	var e_date = new Date(query['end_date']);
 	var instr_list = query['instr_list'];
 	var tpc_list = query['tpc_list'];
 	var newsTester = new Array();
-	async.eachSeries(allNews, 
+	async.each(allNews, 
 		function(news, cb){
 			var date = new Date(news['date']);
 			var hasList = false;
 			var pushed = false;
+			// tests whether the current news article matches the query requirements
 			if(s_date <= date && e_date >= date){
 				if(instr_list.length == 0 && tpc_list.length == 0){
 					newsTester.push(news);
@@ -141,11 +175,14 @@ function getDateRange(cb){
 	});
 }
 
+//not using anymore, cant be bothered
 function randomQuery(cb){
 	async.parallel([
 		function(callback){
 			getDateRange(function(range){
-
+				var start = new Date(range['min_date']);
+				var end = new Date(range['max_date']);
+				var diff = (start.getDate()-end.getDate())/1000
 				callback(null, {'start_date':range['min_date'], 
 					'end_date':range['max_date']});
 			})
@@ -153,14 +190,24 @@ function randomQuery(cb){
 		function(callback){
 			request({uri:'http://pacificpygmyowl.herokuapp.com/api/tpc_list', method:'GET'}, 
 			function(err, res, body){
-				callback(null, JSON.parse(res.body));
+				ns = res.body.replace(/\r/gi, "");
+				tpc_list = JSON.parse(ns);
+				callback(null, tpc_list);
+			})
+		},
+		function(callback){
+			request({uri:'http://pacificpygmyowl.herokuapp.com/api/instr_list', method:'GET'}, 
+			function(err, res, body){
+				ns = res.body.replace(/\r/gi, "");
+				instr_list = JSON.parse(ns);
+				callback(null, instr_list);
 			})
 		}
 	], 
 	function(err, result){
 		var query = result[0];
 		query['tpc_list'] = result[1];
-		query['instr_list'] = [];
+		query['instr_list'] = result[2];
 		cb(query)
 	});
 }
