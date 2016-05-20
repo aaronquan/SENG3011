6d8352a3c17fe327b254b4efdf82c51e04260b95
pauldong <z5020383@student.unsw.edu.au>
2016-05-20 23:40:28 +1000

    JSON request form fixes

--- routes/api_helper/output.js
+++ routes/api_helper/output.js
@@ -19,27 +19,18 @@ function search(query, callback) {
 }
 function dateSearch(query, callback, last) {
     callbackG = callback;
-    console.log(query.range_start);
-    console.log(query.range_length);
-//    last = ObjectId('000000000000000000000000');
-//    News.find({"_id": {"$gte": last.toString()}}, function (err, post) {
-//        console.log(post);
-//        });
-    if (query.instr_list != "" && query.tpc_list != "") {
+    console.log(query.instr_list);
+    if ((query.instr_list != undefined && query.tpc_list != undefined) && (query.instr_list != "" && query.tpc_list != "")) {
         var instr_split = query.instr_list.toString().split(/[,\ *]/);
         var tpc_split = query.tpc_list.toString().split(/[,\ *]/);
-        //News.find({ "_id": {"$gt": last}, "date": {"$gte": query.start_date, "$lte": query.end_date}, "instr_list": {"$in": instr_split}, "tpc_list": {"$in": tpc_split}}, '-_id -__v', call).limit(query.range_length);
         News.find({"date": {"$gte": query.start_date, "$lte": query.end_date}, "instr_list": {"$in": instr_split}, "tpc_list": {"$in": tpc_split}}, '-_id -__v', call).skip(query.range_start).limit(query.range_length);
-    } else if (query.instr_list != "") {
+    } else if (query.instr_list != "" && query.instr_list != undefined) {
         var instr_split = query.instr_list.toString().split(/[,\ *]/);
-        //News.find({ "_id": {"$gt": last}, "date": {"$gte": query.start_date, "$lte": query.end_date}, "instr_list": {"$in": instr_split}}, '-__v -_id', call).limit(query.range_length);
         News.find({"date": {"$gte": query.start_date, "$lte": query.end_date}, "instr_list": {"$in": instr_split}}, '-__v -_id', call).skip(query.range_start).limit(query.range_length);
-    } else if (query.tpc_list != "") {
+    } else if (query.tpc_list != "" && query.tpc_list != undefined) {
         var tpc_split = query.tpc_list.toString().split(/[,\ *]/);
-        //News.find({ "_id": {"$gt": last}, "date": {"$gte": query.start_date, "$lte": query.end_date}, "tpc_list": {"$in": tpc_split}}, '-_id -__v', call).limit(query.range_length);
         News.find({"date": {"$gte": query.start_date, "$lte": query.end_date}, "tpc_list": {"$in": tpc_split}}, '-_id -__v', call).skip(query.range_start).limit(query.range_length);
     } else { 
-        //News.find({ "_id": {"$gt": last}, "date": {"$gte": query.start_date, "$lte": query.end_date}}, '-_id -__v', call).limit(query.range_length);
         News.find({"date": {"$gte": query.start_date, "$lte": query.end_date}}, '-_id -__v', call).skip(query.range_start).limit(query.range_length);
     }
 }
